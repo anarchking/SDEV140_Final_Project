94377b21184805474f24a698d55768a52d09d134
Stephen Littman <Stephen.littman7912@gmail.com>
2025-05-10 17:31:20 -0400

    Update collect_all_images.py
    Made tolerance a Constraint to remove magic number.

--- collect_all_images.py
+++ collect_all_images.py
@@ -8,6 +8,8 @@ import shutil
 #reference_image = ""
 #output_folder = ""
 
+TOLERANCE = 0.6
+
 
 def find_matching_faces(image_folder, reference_image_path, output_folder):
     """
@@ -21,19 +23,24 @@ def find_matching_faces(image_folder, reference_image_path, output_folder):
     """
     
     reference_image = face_recognition.load_image_file(reference_image_path)
-    reference_encoding = face_recognition.face_encodings(reference_image)[0] 
+    reference_encoding = face_recognition.face_encodings(reference_image)[0]
+    # Count for each image processed
     image_count = 0
+
     for filename in os.listdir(image_folder):
         if filename.endswith((".jpg", ".jpeg", ".png")):
+            # Increment the image count
             image_count += 1
             print(f"{filename} is the {image_count} file")
+            # Load the image
             image_path = os.path.join(image_folder, filename)
             image = face_recognition.load_image_file(image_path)
             face_encodings = face_recognition.face_encodings(image)
-            
+            # Check if any faces are found
             if face_encodings:
                 for encoding in face_encodings:
-                    match = face_recognition.compare_faces([reference_encoding], encoding, tolerance=0.6)
+                    match = face_recognition.compare_faces([reference_encoding], encoding, tolerance=TOLERANCE)
+                    # If a match is found, copy the image to the output folder
                     if match[0]: 
                         new_path = os.path.join(output_folder, filename)
                         shutil.copy(image_path, new_path)
